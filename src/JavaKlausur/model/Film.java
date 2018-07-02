4fb7280b0b6c4cd7e14dc1b5662c58aa0e4bae33
spossner <projects@possner.de>
2018-07-02 17:49:33 +0200

    comments to discuss with Nils + Methode um zu prüfen, ob einer von den angegebenen Personen Regisseur des Films ist

--- src/JavaKlausur/model/Film.java
+++ src/JavaKlausur/model/Film.java
@@ -4,91 +4,97 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Film {
-	
-	private int id;
-	private String name;
-	private String beschreibung;
-	private String release;
-	private int votes;
-	private double rating;
-	private List<Schauspieler> schauspieler = new ArrayList<Schauspieler>();
-	private List<Regisseur> regisseure = new ArrayList<Regisseur>();
-	private List<Genre> genre = new ArrayList<Genre>();
-	private List<Bewertung> bewertungen = new ArrayList<>();
-	
-	
-	public Film(int id, String name, String beschreibung, String release, int votes, double rating ) {
-		this.id = id;
-		this.name = name;
-		this.beschreibung = beschreibung;
-		this.release = release;
-		this.votes = votes;
-		this.rating = rating;
-	}
-	
-	public Integer getId(){
-		return id;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public List<Schauspieler> getSchauspieler(){
-		return schauspieler;
-	}
-	
-	public List<Regisseur> getRegisseure(){
-		return regisseure;
-	}
-	
-	public String getBeschreibung() {
-		return beschreibung;
-	}
-	
-	public List<Genre> getGenre() {
-		return genre;
-	}
-	
-	public String getRelease() {
-		return release;
-	}
-	
-	public int getVotes() {
-		return votes;
-	}
-	
-	public double getRating() {
-		return rating;
-	}
 
-	public List<Bewertung> getBewertungen() {
-		return this.bewertungen;
-	}
-	
-	public void addSchauspieler(Schauspieler actor) {
-		schauspieler.add(actor);
-	}
-	
-	public void addRegisseur(Regisseur regisseur) {
-		regisseure.add(regisseur);
-	}
-	
-	public void addGenre(Genre genre) {
-		this.genre.add(genre);
-	}
+    private int id;
+    private String name;
+    private String beschreibung;
+    private String release;
+    private int votes;
+    private double rating;
+    private List<Schauspieler> schauspieler = new ArrayList<Schauspieler>();
+    private List<Regisseur> regisseure = new ArrayList<Regisseur>();
+    private List<Genre> genre = new ArrayList<Genre>();
+    private List<Bewertung> bewertungen = new ArrayList<>();
 
-	public void addBewertung(Bewertung bewertung) {
-		this.bewertungen.add(bewertung);
-	}
 
-	@Override
-	public String toString() {
-		return "Film{" +
-				"id=" + id +
-				", name='" + name + '\'' +
-				'}';
-	}
+    public Film(int id, String name, String beschreibung, String release, int votes, double rating) {
+        this.id = id;
+        this.name = name;
+        this.beschreibung = beschreibung;
+        this.release = release;
+        this.votes = votes;
+        this.rating = rating;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Schauspieler> getSchauspieler() {
+        return schauspieler;
+    }
+
+    public List<Regisseur> getRegisseure() {
+        return regisseure;
+    }
+
+    public String getBeschreibung() {
+        return beschreibung;
+    }
+
+    public List<Genre> getGenre() {
+        return genre;
+    }
+
+    public String getRelease() {
+        return release;
+    }
+
+    public int getVotes() {
+        return votes;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public List<Bewertung> getBewertungen() {
+        return this.bewertungen;
+    }
+
+    public void addSchauspieler(Schauspieler actor) {
+        schauspieler.add(actor);
+    }
+
+    public void addRegisseur(Regisseur regisseur) {
+        regisseure.add(regisseur);
+    }
+
+    public void addGenre(Genre genre) {
+        this.genre.add(genre);
+    }
+
+    public void addBewertung(Bewertung bewertung) {
+        this.bewertungen.add(bewertung);
+    }
+
+    // @TODO toString.. was ist das und was macht das?
+    @Override
+    public String toString() {
+        return "Film{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public boolean hatRegisseur(String[] namen) {
+        //@TODO prüfen, ob min. einer der übergebenen Namen Regisseur des Films ist
+        return false;
+    }
 }
 
 
