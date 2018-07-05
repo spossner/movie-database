5a33ad9b3345ec223aeef6844bac75f3c5c39d0b
spossner <projects@possner.de>
2018-07-05 17:19:38 +0200

    Refactored utils methods into own class

---
+++ test/JavaKlausur/UtilsTest.java
@@ -0,0 +1,25 @@
+package JavaKlausur;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class UtilsTest {
+    @Test
+    public void testSplit() {
+        String line = "--genre='Thriller,Action'";// --actor='Keanu Reeves,Jason Statham' --film='Matrix' --limit=10";
+        String[] words = Utils.splitAndTrimQuotes(line, '=');
+        assertEquals(2, words.length);
+        for (String s : words) {
+            System.out.println(s);
+        }
+
+        String[] genres = Utils.splitAndTrimQuotes(words[1],',');
+        assertEquals(2, genres.length);
+        for (String s : genres) {
+            System.out.println(s);
+        }
+    }
+}
