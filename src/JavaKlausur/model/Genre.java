fd5be8d831d7bc588cb1987c8ac28eb5e3a5f6b5
spossner <1.sepp0>
2018-07-05 06:43:38 +0200

    cleanup

--- src/JavaKlausur/model/Genre.java
+++ src/JavaKlausur/model/Genre.java
@@ -5,10 +5,14 @@ import java.util.List;
 
 public class Genre {
     private String name;
-    private List<Film> filme = new ArrayList<Film>();
+    private List<Film> filme = new ArrayList<>();
 
     public Genre(String name) {
         this.name = name;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public void addFilm(Film film) {
@@ -17,9 +21,5 @@ public class Genre {
 
     public List<Film> getFilme() {
         return filme;
-    }
-
-    public String getName() {
-        return name;
     }
 }
