e4522351a0200cdde3a742258eeb3b94be570169
spossner <projects@possner.de>
2018-07-22 19:36:35 +0200

    use generic splitAndTrim from utils

--- test/JavaKlausur/UtilsTest.java
+++ test/JavaKlausur/UtilsTest.java
@@ -10,13 +10,13 @@ public class UtilsTest {
     @Test
     public void testSplit() {
         String line = "--genre='Thriller,Action'";// --actor='Keanu Reeves,Jason Statham' --film='Matrix' --limit=10";
-        String[] words = Utils.splitAndTrimQuotes(line, '=');
+        String[] words = Utils.splitAndTrimQuotes(line, "=");
         assertEquals(2, words.length);
         for (String s : words) {
             System.out.println(s);
         }
 
-        String[] genres = Utils.splitAndTrimQuotes(words[1],',');
+        String[] genres = Utils.splitAndTrimQuotes(words[1],",");
         assertEquals(2, genres.length);
         for (String s : genres) {
             System.out.println(s);
